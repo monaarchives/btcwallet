8cde52d68c7ebd175cd6ddf0c9b25ae3ffe4b6ba
wakiyamap <wakiyamap@gmail.com>
2020-12-05 08:14:22 +0900

    Fix import modules

--- wallet/history.go
+++ wallet/history.go
@@ -8,9 +8,9 @@ import (
 	"fmt"
 
 	"github.com/btcsuite/btcd/chaincfg/chainhash"
-	"github.com/btcsuite/btcwallet/waddrmgr"
-	"github.com/btcsuite/btcwallet/walletdb"
-	"github.com/btcsuite/btcwallet/wtxmgr"
+	"github.com/monaarchives/btcwallet/waddrmgr"
+	"github.com/monaarchives/btcwallet/walletdb"
+	"github.com/monaarchives/btcwallet/wtxmgr"
 )
 
 var (
